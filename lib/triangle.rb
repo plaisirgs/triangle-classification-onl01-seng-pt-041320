b12618998d2c20ead07c86187e8ff94e75cdc762
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-05-25 22:19:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,75 @@
+# class Triangle
+
+#   attr_accessor :s1, :s2, :s3, :kind
+  
+#   def initialize(s1, s2, s3)
+#     @s1= s1
+#     @s2= s2
+#     @s3 = s3
+#   end  
+    
+#   def kind
+#     if (s1 * s2 * s3) == 0 || (s1 + s2) <= s3 || (s2 + s3) <= s1 || (s3 + s1) <= s2
+#       begin
+#         raise TriangleError
+#       end
+    
+#     elsif s1 == s2 && s1 == s3
+#       self.kind = :equilateral
+    
+#     elsif s1 == s2 || s1 == s3 || s2 == s3
+#       self.kind = :isosceles
+      
+#     else
+#       self.kind = :scalene
+#       end
+#     end
+#   end 
+  
+# class TriangleError < StandardError
+#   def message
+#   end
+# end
+
 class Triangle
-  # write code here
+  # write code here\
+
+  def initialize(s1, s2, s3)
+
+  
+    if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 >= (s1 + s2)
+      raise TriangleError
+    end
+
+    if s1 <= 0 || s2 <= 0 ||s3 <= 0
+      raise TriangleError
+    end
+
+    if s1 == nil || s2 == nil ||s3 == nil
+      raise TriangleError
+    end
+
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+    if @s1 == @s2 && @s1 == @s3
+      return :equilateral
+    elsif @s1 == @s2 || @s2 == @s3 || @s3 == @s1
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+end
+
+
+
+class TriangleError < StandardError
+  def message
+    "This is not a valid triangle."
+  end
 end
